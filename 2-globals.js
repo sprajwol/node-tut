ac03172158e4f66bca5f8eacc1691e21d485158a
Prajwol Shakya <shakyaprajwol96@gmail.com>
2022-04-08 14:24:05 +0545

    5-utils.js

---
+++ 2-globals.js
@@ -0,0 +1,12 @@
+// GLOBAL   - NO WINDOW
+
+// __dirname    - path to dicrectory
+// __filename   - file name
+// require      - function to use modules (CommonJS)
+// module       - info about current module (file)
+// process      - info about env where the program is being executed
+
+console.log(__dirname)
+setInterval(()=>{
+    console.log('Hey World')
+}, 1000)
