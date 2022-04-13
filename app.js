b5bc7047752de0bc49c7749f233df59f02a7a7e3
Prajwol Shakya <shakyaprajwol96@gmail.com>
2022-04-13 08:42:33 +0545

    mind grenade

--- app.js
+++ app.js
@@ -3,7 +3,9 @@
 
 const names = require('./4-names');
 const sayHi = require('./5-utils');
+const data = require('./6-alternative-flavor');
+require('./7-mind-grenade');
 
-sayHi("Susan")
-sayHi(names.john)
-sayHi(names.peter)
+// sayHi("Susan")
+// sayHi(names.john)
+// sayHi(names.peter)
