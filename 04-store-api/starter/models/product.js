2e10f92993f83ce9f4f6834fc0b703b71a710f02
Prajwol Shakya <shakyaprajwol96@gmail.com>
2022-05-17 10:38:43 +0545

    populate DB

--- 04-store-api/starter/models/product.js
+++ 04-store-api/starter/models/product.js
@@ -24,7 +24,7 @@ const productSchema = new mongoose.Schema({
 	company: {
 		type: String,
         enum:{
-            values:['ikea', 'liddy', 'caressa', 'marcoa'],
+            values:['ikea', 'liddy', 'caressa', 'marcos'],
             message:'{VALUE} is not supported'
         }
 		// enum: ['ikea', 'liddy', 'caressa', 'marcoa'],
