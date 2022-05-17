75ca1aace8c6db50aeb3321798dd6d987f6bf17b
Prajwol Shakya <shakyaprajwol96@gmail.com>
2022-05-17 09:32:34 +0545

    model setup

---
+++ 04-store-api/starter/models/product.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, 'product name must be provided'],
+	},
+	price: {
+		type: Number,
+		required: [true, 'price name must be provided'],
+	},
+	featured: {
+		type: Boolean,
+		default: false,
+	},
+	rating: {
+		type: Number,
+		default: 4.5,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now(),
+	},
+	company: {
+		type: String,
+        enum:{
+            values:['ikea', 'liddy', 'caressa', 'marcoa'],
+            message:'{VALUE} is not supported'
+        }
+		// enum: ['ikea', 'liddy', 'caressa', 'marcoa'],
+	},
+});
+
+module.exports = mongoose.model('Product', productSchema)
