4fd4d07079ec56122d3643d7ba5e932c37ea6326
Prajwol Shakya <shakyaprajwol96@gmail.com>
2022-05-17 09:01:16 +0545

    connect db

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const connectDB = require('./db/connect')
+
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
 
@@ -25,6 +27,7 @@ const port = process.env.PORT || 3000;
 start = async () => {
 	try {
 		// ConnectDB
+        await connectDB(process.env.MONGO_URI)
 		app.listen(port, console.log(`Server is listening on port ${port}...`));
 	} catch (error) {
 		console.log(error);
