8ef5d5e47aefb5a992751a4bad908c86fb77443a
Prajwol Shakya <shakyaprajwol96@gmail.com>
2022-05-19 10:01:08 +0545

    pagination

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,9 +1,52 @@
+const Product = require('../models/product');
+
 const getAllProductsStatic = async (req, res) => {
-	res.status(200).json({ msg: 'product testing route' });
+	const search = 'ab';
+	const products = await Product.find({})
+		.sort('name')
+		.select('name price')
+		.limit(10)
+		.skip(1);
+	res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-	res.status(200).json({ msg: 'product route' });
+	const { featured, company, name, sort, fields } = req.query;
+	const queryObject = {};
+	if (featured) {
+		queryObject.featured = featured === 'true' ? true : false;
+	}
+	if (company) {
+		queryObject.company = company;
+	}
+	if (name) {
+		queryObject.name = { $regex: name, $options: 'i' };
+	}
+	// console.log(queryObject)
+	let result = Product.find(queryObject);
+	//sort
+	if (sort) {
+		const sortList = sort.split(',').join(' ');
+		result = result.sort(sortList);
+	} else {
+		result = result.sort('createdAt');
+	}
+
+	if (fields) {
+		const fieldsList = fields.split(',').join(' ');
+		result = result.select(fieldsList);
+	}
+
+	const page = Number(req.query.page) || 1;
+	const limit = Number(req.query.page) || 10;
+	const skip = (page - 1) * limit;
+
+	result = result.skip(skip).limit(limit);
+	// 23
+	// 4 7 7 7 2
+
+	const products = await result;
+	res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
