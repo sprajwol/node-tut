f12dc11f06fde29207fb355ee3d14f1dafe07bdc
Prajwol Shakya <shakyaprajwol96@gmail.com>
2022-07-05 16:17:51 +0545

    jwt basics done

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,13 +1,13 @@
 const jwt = require('jsonwebtoken');
 
-const CustomAPIError = require('../errors/custom-error');
+const { BadRequestError } = require('../errors');
 
 const login = async (req, res) => {
 	const { username, password } = req.body;
 	// console.log(username, password);
 
 	if (!username || !password) {
-		throw new CustomAPIError('Please provide username and password.', 400);
+		throw new BadRequestError('Please provide username and password.');
 	}
 	const id = new Date().getDate();
 
@@ -18,7 +18,7 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-	console.log("dashboard", req.user)
+	console.log('dashboard', req.user);
 	const luckyNumber = Math.floor(Math.random() * 100);
 	res.status(200).json({
 		msg: `Hello, ${req.user.username}`,
