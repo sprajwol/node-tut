dc0ff7f77d5cc4479a441b3b7cca902079502584
Prajwol Shakya <shakyaprajwol96@gmail.com>
2022-07-05 11:30:04 +0545

    authenticationMiddleware 6:13:20

---
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+
+const CustomAPIError = require('../errors/custom-error');
+
+const login = async (req, res) => {
+	const { username, password } = req.body;
+	// console.log(username, password);
+
+	if (!username || !password) {
+		throw new CustomAPIError('Please provide username and password.', 400);
+	}
+	const id = new Date().getDate();
+
+	const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+		expiresIn: '30d',
+	});
+	res.status(200).json({ msg: 'User created', token: token });
+};
+
+const dashboard = async (req, res) => {
+	console.log("dashboard", req.user)
+	const luckyNumber = Math.floor(Math.random() * 100);
+	res.status(200).json({
+		msg: `Hello, ${req.user.username}`,
+		secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+	});
+};
+
+module.exports = {
+	login,
+	dashboard,
+};
